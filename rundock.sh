ce53ad9bb068fc9b75280f79b793b0893b45b4b9
pncapm <pncapm@outlook.com>
2019-01-07 20:58:31 -0500

    !

--- rundock.sh
+++ rundock.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-docker stop optorch
-docker rm optorch
-docker run --name optorch --network host --restart unless-stopped -d pncapm/optorch
+docker run -d --name watchtower --restart always -v /var/run/docker.sock:/var/run/docker.sock v2tec/watchtower
+docker run -d --name optorch --network host --restart always pncapm/optorch
