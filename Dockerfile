55c9a48df172e034ee52b7ee9578d1d2aa9085f9
pncapm <1157wcl@gmail.com>
2019-01-08 00:25:56 +0000

    Building docker for arm

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:11
+FROM arm32v7/node:11
 WORKDIR /usr/src/app
 COPY package*.json ./
 RUN npm install --loglevel=error
--- rundock.sh
+++ rundock.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+docker run -d --name watchtower -v /var/run/docker.sock:/var/run/docker.sock v2tec/watchtower
 docker stop optorch
 docker rm optorch
 docker run --name optorch --network host --restart unless-stopped -d pncapm/optorch
