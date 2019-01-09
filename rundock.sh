227d4d27e03be16867f3ccee00783efe80cfd7f8
pncapm <1157wcl@gmail.com>
2019-01-09 05:30:33 +0000

    monster

--- rundock.sh
+++ rundock.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-docker run -d --name watchtower --restart always -v /var/run/docker.sock:/var/run/docker.sock v2tec/watchtower
-docker run -d --name optorch --network host --restart always pncapm/optorch
+docker run -d --name watchtower --restart always -v /var/run/docker.sock:/var/run/docker.sock v2tec/watchtower:armhf-latest
+docker run -d --name optorch --network host --restart always -v /pncapm/optorch
+
 
