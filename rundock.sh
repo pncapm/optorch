36b65b5f0ee2dbe4e436586753a1e1fb8c7e2da5
pncapm <1157wcl@gmail.com>
2019-01-09 13:45:12 +0000

    more tweaks to docker commands.  APPPPARENTLY spelling is important.

--- rundock.sh
+++ rundock.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker run -d --name watchtower --restart always -v /var/run/docker.sock:/var/run/docker.sock v2tec/watchtower:armhf-latest
-docker run -d --name optorch --network host --restart always -v /home/pi/logs:/logs pncapm/optorch
+docker run -d --name optorch --network host --restart always -v /home/pi/logs:/usr/src/app/logs pncapm/optorch
 
 
